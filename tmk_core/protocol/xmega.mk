c97df2757a875904d6ddb2e44e33fd56daf4f678
mewa <marcin.k.chmiel@gmail.com>
2018-11-04 22:59:06 +0100

    Add required USB sources for XMEGA protocol

--- tmk_core/protocol/xmega.mk
+++ tmk_core/protocol/xmega.mk
@@ -2,5 +2,17 @@ XMEGA_DIR = protocol/xmega
 
 SRC += $(XMEGA_DIR)/main_xmega.c
 
+ASF_SRC += $(LIB_ASF)/common/services/clock/xmega/sysclk.c
+ASF_SRC += $(LIB_ASF)/common/services/sleepmgr/xmega/sleepmgr.c
+ASF_SRC += $(LIB_ASF)/common/services/usb/class/hid/device/generic/udi_hid_generic.c
+ASF_SRC += $(LIB_ASF)/common/services/usb/class/hid/device/generic/udi_hid_generic_desc.c
+ASF_SRC += $(LIB_ASF)/common/services/usb/class/hid/device/udi_hid.c
+ASF_SRC += $(LIB_ASF)/common/services/usb/udc/udc.c
+ASF_SRC += $(LIB_ASF)/xmega/drivers/cpu/ccp.S
+ASF_SRC += $(LIB_ASF)/xmega/drivers/nvm/nvm_asm.S
+ASF_SRC += $(LIB_ASF)/xmega/drivers/usb/usb_device.c
+
+SRC += $(ASF_SRC)
+
 VPATH += $(TMK_DIR)/$(XMEGA_DIR)
 VPATH += $(TMK_DIR)/$(XMEGA_DIR)/config
